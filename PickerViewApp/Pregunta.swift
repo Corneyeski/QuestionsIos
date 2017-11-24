5f4083c2f5cf5489faf5e7de6a8d39f0be87ffa7
DAM <DAM@iMac.local>
2017-11-24 20:16:31 +0100

    acabada

--- PickerViewApp/Pregunta.swift
+++ PickerViewApp/Pregunta.swift
@@ -10,31 +10,37 @@ import Foundation
 
 class Pregunta{
     
-    private var pregunta: String
-    private var respuesta: String
-    private var respuesta2: String
-    private var respuesta3: String
-    private var respuesta4: String
-    private var realizada:Bool
-    private var correcta:Int
+    var pregunta: String
+    var respuestas: [String]
+    /*var respuesta: String
+    var respuesta2: String
+    var respuesta3: String
+    var respuesta4: String*/
+    var realizada:Bool
+    var correcta:Int
+    var acertada:Bool
     
     init(){
         self.pregunta = ""
-        self.respuesta = ""
+        self.respuestas = []
+        /* self.respuesta = ""
         self.respuesta2 = ""
         self.respuesta3 = ""
-        self.respuesta4 = ""
+        self.respuesta4 = "" */
         self.realizada = false
         self.correcta = 0
+        self.acertada = false;
     }
     
     init(pregunta: String, respuesta:String, respuesta2:String, respuesta3:String, respuesta4:String, correcta:Int) {
         self.pregunta = pregunta
-        self.respuesta = respuesta
+        self.respuestas = [respuesta,respuesta2,respuesta3,respuesta4]
+        /*self.respuesta = respuesta
         self.respuesta2 = respuesta2
         self.respuesta3 = respuesta3
-        self.respuesta4 = respuesta4
+        self.respuesta4 = respuesta4*/
         self.realizada = false
         self.correcta = correcta
+        self.acertada = false;
     }
 }
--- PickerViewApp/ViewController.swift
+++ PickerViewApp/ViewController.swift
@@ -1,14 +1,4 @@
-//
-//  ViewController.swift
-//  PickerViewApp
-//
-//  Created by DAM on 15/11/17.
-//  Copyright © 2017 DAM. All rights reserved.
-//
-
 import UIKit
-
-
 
 
 class ViewController: UIViewController, UIPickerViewDataSource,
@@ -16,37 +6,47 @@ class ViewController: UIViewController, UIPickerViewDataSource,
     
     var groups:[Tema] = []
     var who = 0
+    var question = Pregunta()
+    var tema = Tema()
+    var points:Int = 0
+    var btnActive = false
+    
+    var randomSelected = 0
     
     @IBOutlet weak var pickerView: UIPickerView!
-    @IBOutlet weak var pregunta: UITextField!
     @IBOutlet weak var start: UIButton!
-    @IBOutlet weak var resa: UITextField!
-    @IBOutlet weak var resb: UITextField!
-    @IBOutlet weak var resc: UITextField!
-    @IBOutlet weak var resd: UITextField!
+    @IBOutlet weak var pregunta: UITextView!
+    @IBOutlet weak var resa: UITextView!
+    @IBOutlet weak var resb: UITextView!
+    @IBOutlet weak var resc: UITextView!
+    @IBOutlet weak var resd: UITextView!
+    @IBOutlet weak var puntos: UITextField!
+    @IBOutlet weak var btna: UIButton!
+    @IBOutlet weak var btnb: UIButton!
+    @IBOutlet weak var btnc: UIButton!
+    @IBOutlet weak var btnd: UIButton!
     
     @IBAction func startQuestion(_ sender: Any) {
         
-        var tema = Tema(object: groups[who])
+        cleanBackgroundcolor()
         
-        switch groups[who].nombre {
-        case "juegos":
-            print("hry")
-            
-        case "series":
-            print("series")
-            
-        default:
-            print("nada")
-        }
-        
-        //hacer de forma dinamica el mostrar la pregunta
+        tema = Tema(object: groups[who])
         
         if !tema.complete && tema.preguntas.count > 0 {
             
-            var random = Int(arc4random_uniform(UInt32(tema.preguntas.count - 0 + 1)) +  0)
+            var ask = false
             
+            repeat {
+                let random = Int(arc4random_uniform(UInt32((tema.preguntas.count-1) - 0 + 1)) +  0)
             
+                if !tema.preguntas[random].realizada {
+                    ask = true
+                    question = tema.preguntas[random]
+                    randomSelected = random
+                }
+            }while !ask
+            
+            complete()
             
         } else {
             
@@ -54,30 +54,57 @@ class ViewController: UIViewController, UIPickerViewDataSource,
         
     }
     
+    //Comprobar respuesta
+    @IBAction func checkAnswer(_ sender: Any) {
+        
+        if (sender as AnyObject).tag+1 == question.correcta {
+            question.acertada = true
+            points += 1
+            puntos.text = "PUNTOS: " + String(points)
+            
+            checkTema()
+            isCorrect(answer: (sender as AnyObject).tag, color: UIColor.green)
+        }else {
+            checkTema()
+            isCorrect(answer: (sender as AnyObject).tag, color: UIColor.red)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         rellenar()
-        
-        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
 
-
-
-
-//Funcion pra crear las preguntas basicas de cada tema
+    //Funcion pra crear las preguntas basicas de cada tema
     func rellenar(){
         
         //Juegos
         var juegos:[Pregunta]
         
-        let j1 = Pregunta(pregunta: "A que compañia pertenece Mario odyssey?", respuesta: "Nintendo", respuesta2: "SEGA", respuesta3: "EA", respuesta4: "Atari", correcta: 1)
-        let j2 = Pregunta(pregunta: "Que dios grigo tiene una saga?", respuesta: "Fenrir", respuesta2: "Zeus", respuesta3: "Kratos", respuesta4: "Jesus", correcta: 3)
-        let j3 = Pregunta(pregunta: "Cual es la consola mas vendida de todos los tiempos?", respuesta: "PS2", respuesta2: "PS3", respuesta3: "super nintendo", respuesta4: "Xbox360", correcta: 1)
+        let j1 = Pregunta(pregunta: "A que compañia pertenece Mario odyssey?",
+                          respuesta: "Nintendo",
+                          respuesta2: "SEGA",
+                          respuesta3: "EA",
+                          respuesta4: "Atari",
+                          correcta: 1)
+        
+        let j2 = Pregunta(pregunta: "Que dios grigo tiene una saga?",
+                          respuesta: "Fenrir",
+                          respuesta2: "Zeus",
+                          respuesta3: "Kratos",
+                          respuesta4: "Jesus",
+                          correcta: 3)
+        
+        let j3 = Pregunta(pregunta: "Cual es la consola mas vendida de todos los tiempos?", respuesta: "PS2",
+                          respuesta2: "PS3",
+                          respuesta3: "super nintendo",
+                          respuesta4: "Xbox360",
+                          correcta: 1)
         
         juegos = [j1,j2,j3]
         groups.append(Tema(nombre: "juegos",preguntas: juegos))
@@ -85,13 +112,122 @@ class ViewController: UIViewController, UIPickerViewDataSource,
         //Series
         var series:[Pregunta]
         
-        let s1 = Pregunta(pregunta: "En Rick&Morty, Cuantas versiones de rick hay en el multiverso?", respuesta: "ninguno", respuesta2: "millones", respuesta3: "wubba-lubba-dab-dab", respuesta4: "infinitos", correcta: 4)
-        let s2 = Pregunta(pregunta: "En Dr.who, Que es el silencio?", respuesta: "Una orden destinada a matar al doctor", respuesta2: "Una orden destinada a evitar que el doctor diga su nombre", respuesta3: "Un grupo de alienigenas que no se pueden recordar", respuesta4: "una orden religiosa", correcta: 2)
-        let s3 = Pregunta(pregunta: "En waliking dead, quien mata a Gleen?", respuesta: "Rick", respuesta2: "Nigan", respuesta3: "Mishon", respuesta4: "Un soldado de nigan", correcta: 2)
+        let s1 = Pregunta(pregunta: "En Rick&Morty, Cuantas versiones de rick hay en el multiverso?", respuesta: "ninguno",
+                          respuesta2: "millones",
+                          respuesta3: "wubba-lubba-dab-dab",
+                          respuesta4: "infinitos",
+                          correcta: 4)
+        
+        let s2 = Pregunta(pregunta: "En Dr.who, Que es el silencio?",
+                          respuesta: "Una orden destinada a matar al doctor",
+                          respuesta2: "Una orden destinada a evitar que el doctor diga su nombre",
+                          respuesta3: "Un grupo de alienigenas que no se pueden recordar",
+                          respuesta4: "una orden religiosa",
+                          correcta: 2)
+        
+        let s3 = Pregunta(pregunta: "En waliking dead, quien mata a Gleen?",
+                          respuesta: "Rick",
+                          respuesta2: "Nigan",
+                          respuesta3: "Mishon",
+                          respuesta4: "Un soldado de nigan",
+                          correcta: 2)
         
         series = [s1,s2,s3]
         groups.append(Tema(nombre:"series",preguntas: series))
     }
+    
+    //Rellenar pregunta
+    func complete() {
+        pregunta.text = question.pregunta
+        resa.text = "A - " + question.respuestas[0]
+        resb.text = "B - " + question.respuestas[1]
+        resc.text = "C - " + question.respuestas[2]
+        resd.text = "D - " + question.respuestas[3]
+        hideButtons()
+    }
+    
+    //Actualizar pregunta y comprobar si el tema esta lleno
+    func checkTema() {
+        question.realizada = true
+        tema.preguntas[randomSelected] = question
+        
+        var check = true
+        
+        for quest in tema.preguntas {
+            if !quest.realizada {
+                check = false
+                break
+            }
+        }
+        
+        if check {
+            cleanBackgroundcolor()
+            tema.complete = true
+            
+            resa.text = ""
+            resb.text = ""
+            resc.text = ""
+            resd.text = ""
+            
+            pregunta.text = "TODAS LAS PREGUNTAS DE ESTE TEMA HAN SIDO RESPONDIDAS"
+            
+        }
+        groups[who] = tema
+
+        hideButtons()
+        
+    }
+    
+    //Esconder o mostrar botones a necesidad (y quitar background si es necesario de texto)
+    func hideButtons() {
+        if !btnActive {
+            btna.isHidden = false;
+            btnb.isHidden = false;
+            btnc.isHidden = false;
+            btnd.isHidden = false;
+            
+            btnActive = true
+        }else {
+            btna.isHidden = true;
+            btnb.isHidden = true;
+            btnc.isHidden = true;
+            btnd.isHidden = true;
+            
+            
+            btnActive = false
+        }
+    }
+    
+    //Poner background rojo/verde segun lo que toque
+    
+    func isCorrect(answer:Int, color:UIColor) {
+        switch answer {
+        case 0:
+            print(0)
+            resa.backgroundColor = color
+        case 1:
+            print(1)
+            resb.backgroundColor = color
+        case 2:
+            print(2)
+            resc.backgroundColor = color
+        case 3:
+            print(3)
+            resd.backgroundColor = color
+        default:
+            print("jaja")
+        }
+    }
+    
+    //Limpiar el background color
+    func cleanBackgroundcolor() {
+        resa.backgroundColor = UIColor.clear
+        resb.backgroundColor = UIColor.clear
+        resc.backgroundColor = UIColor.clear
+        resd.backgroundColor = UIColor.clear
+    }
+    
+    //PICKERVIEW SETTINGS
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -107,6 +243,5 @@ class ViewController: UIViewController, UIPickerViewDataSource,
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         who = row
-        startQuestion(startQuestion((Any).self))
     }
 }
